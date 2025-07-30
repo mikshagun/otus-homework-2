944ee517277da272aed0715c61361c1df8a4a9fe
mikshagun <mshagun@java-qa.ru>
2025-07-30 19:29:52 +0300

    add dockerfile and application

---
+++ src/main/java/otus_homework/Application.java
@@ -0,0 +1,23 @@
+package otus_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+class Controller {
+
+    @GetMapping("/health")
+    public String health() {
+        return "{\"status\": \"OK\"}";
+    }
+}
